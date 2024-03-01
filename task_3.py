a9e030d93d46ebfc55d54eb7a093a52f774449ac
Kirill Tregubov <tregubov@algont.ru>
2024-03-01 20:42:49 +0300

    added run Firefox Nigthly (changed default path to firefox.exe)

--- task_3.py
+++ task_3.py
@@ -8,7 +8,7 @@ def browser(request):
     print("\nstart firefox browser for test..")
     browser = webdriver.Firefox()
     # Вызов конкретного браузера
-    # browser = webdriver.Firefox(firefox_binary="c:\\Program Files (x86)\\Nightly\\firefox.exe")
+    # browser = webdriver.Firefox(firefox_binary="c:\\Program Files\\Firefox Nightly\\firefox.exe")
     browser.maximize_window()
     browser.implicitly_wait(2)
     yield browser
