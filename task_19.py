458fb4073f293a1ac22f8ff1a165bc04ca242ac7
Kirill Tregubov <tregubov@algont.ru>
2024-03-29 12:11:31 +0300

    moving page files to subdirectory 'pages'

--- task_19.py
+++ task_19.py
@@ -1,6 +1,6 @@
-from base_page import BasePage
-from cart_page import CartPage
-from main_page import MainPage
+from pages.base_page import BasePage
+from pages.cart_page import CartPage
+from pages.main_page import MainPage
 
 
 def test_task_13(browser):
